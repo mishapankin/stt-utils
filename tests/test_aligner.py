bcf1f7a7794afe67f90e4af336002a10fd4e8ba7
Mikhail Pankin <mishapankin@gmail.com>
2025-11-16 19:21:13 +0300

    Added a test case for from_sequence builder

--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -1,5 +1,10 @@
 import unittest
-from stt_aligner import normalize_word, Transcription, UnprocessedTranscription
+from stt_aligner import (
+    normalize_word,
+    Transcription,
+    UnprocessedTranscription,
+    Timestamp,
+)
 import re
 
 
@@ -11,7 +16,7 @@ class TestAligner(unittest.TestCase):
         self.assertEqual(normalize_word("coöperate"), "cooperate")
         self.assertEqual(normalize_word("São Paulo"), "sao paulo")
 
-    def test_aligner(self):
+    def test_from_unprocessed_transcription(self):
         with open("tests/assets/homer.json", "r") as file:
             data = file.read()
 
@@ -23,6 +28,46 @@ class TestAligner(unittest.TestCase):
             self.assertTrue(re.match(r"\w+", transcription.get_word_by_timestamp(ts)))
             self.assertIsNotNone(ts.start_time)
             self.assertIsNotNone(ts.end_time)
+
+    def test_from_sequence(self):
+        t1 = Transcription(
+            duration=1.0,
+            tokens=["hello", " ", "world"],
+            timestamps=[
+                Timestamp(index=0, start_time=0.0, end_time=0.5),
+                Timestamp(index=2, start_time=0.5, end_time=1.0),
+            ],
+        )
+        t2 = Transcription(
+            duration=2.0,
+            tokens=["foo", " ", "bar"],
+            timestamps=[
+                Timestamp(index=0, start_time=1.0, end_time=1.5),
+                Timestamp(index=2, start_time=1.5, end_time=2.0),
+            ],
+        )
+
+        # Test without a separator
+        combined = Transcription.from_sequence([t1, t2], sep=None)
+        self.assertEqual(combined.duration, 3.0)
+        self.assertEqual(combined.tokens, ["hello", " ", "world", "foo", " ", "bar"])
+        self.assertEqual(len(combined.timestamps), 4)
+        self.assertEqual(combined.timestamps[0].index, 0)
+        self.assertEqual(combined.timestamps[0].start_time, 0.0)
+        self.assertEqual(combined.timestamps[1].index, 2)
+        self.assertEqual(combined.timestamps[1].start_time, 0.5)
+        self.assertEqual(combined.timestamps[2].index, 3)
+        self.assertEqual(combined.timestamps[2].start_time, 1.0)
+        self.assertEqual(combined.timestamps[3].index, 5)
+        self.assertEqual(combined.timestamps[3].start_time, 1.5)
+
+        # Test with a separator
+        combined_sep = Transcription.from_sequence([t1, t2], sep="|")
+        self.assertEqual(combined_sep.duration, 3.0)
+        self.assertEqual(
+            combined_sep.tokens, ["hello", " ", "world", "|", "foo", " ", "bar"]
+        )
+        self.assertEqual(len(combined_sep.timestamps), 4)
 
 
 if __name__ == "__main__":
