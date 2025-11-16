931e875f9a875d8745e7deabd6d04b53d0fffe0c
Mikhail Pankin <mishapankin@gmail.com>
2025-11-16 15:34:59 +0300

    Updated timestamp indexing to allow editing text

--- src/stt_aligner/stt.py
+++ src/stt_aligner/stt.py
@@ -21,83 +21,79 @@ class UnprocessedTranscription(BaseModel):
 
 @dataclass
 class Timestamp:
-    start_index: int
-    end_index: int
+    index: int
 
-    start_time: float = None
-    end_time: float = None
+    start_time: float
+    end_time: float
 
 
 class Transcription:
-    text: str
+    tokens: list[str]
     timestamps: list[Timestamp]
 
     def __init__(self, unprocessed: UnprocessedTranscription):
-        self.text = unprocessed.text
+        text = unprocessed.text
         self.timestamps = []
 
-        original_indicies = []
-        original_words = []
+        self.tokens = [tok for tok in re.split(r"(\w+)", text) if tok != ""]
 
-        for match in re.finditer(r"\w+", self.text):
-            original_indicies.append(match.span())
-            original_words.append(normalize_word(match.group(0)))
-
+        normalized_tokens = [normalize_word(tok) for tok in self.tokens]
         timestamped_words = [normalize_word(w.word) for w in unprocessed.words]
 
-        matcher = difflib.SequenceMatcher(None, original_words, timestamped_words)
+        matcher = difflib.SequenceMatcher(None, normalized_tokens, timestamped_words)
         for match in matcher.get_matching_blocks():
             for i in range(match.size):
                 idx1 = match.a + i
                 idx2 = match.b + i
 
-                start_idx, end_idx = original_indicies[idx1]
                 word_info = unprocessed.words[idx2]
 
                 timestamp = Timestamp(
-                    start_index=start_idx,
-                    end_index=end_idx,
+                    index=idx1,
                     start_time=word_info.start,
                     end_time=word_info.end,
                 )
                 self.timestamps.append(timestamp)
 
-    def _get_preview_markers(self) -> str:
-        t = [" "] * len(self.text)
+    def get_text(self) -> str:
+        return "".join(self.tokens)
 
+    def _get_preview_markers(self) -> str:
+        text = self.get_text()
+        t = [" " * len(c) for c in self.tokens]
         for ts in self.timestamps:
-            for i in range(ts.start_index, ts.end_index):
-                if 0 <= i < len(t):
-                    t[i] = "^"
+            t[ts.index] = "^" * len(self.tokens[ts.index])
 
         return "".join(t)
 
     def get_word_by_timestamp(self, timestamp: Timestamp) -> str:
-        return self.text[timestamp.start_index : timestamp.end_index]
+        return self.tokens[timestamp.index]
 
     def dumps_preview(self) -> str:
-        return self.text + "\n" + self._get_preview_markers()
+        return self.get_text() + "\n" + self._get_preview_markers()
 
     def dumps_preview_wrapped(self, width: int) -> str:
         if width <= 0:
             raise ValueError("Wrapping width must be positive")
 
+        text = self.get_text()
+
         wrapper = textwrap.TextWrapper(
             width=width, break_long_words=True, replace_whitespace=False
         )
-        lines = wrapper.wrap(self.text)
+        lines = wrapper.wrap(text)
 
         pos = 0
         output_lines = []
         markers = self._get_preview_markers()
 
         for line in lines:
-            start = self.text.find(line, pos)
+            start = text.find(line, pos)
             if start == -1:
                 start = pos
-                line = self.text[start : start + min(width, len(self.text) - start)]
+                line = text[start : start + min(width, len(text) - start)]
 
-            output_lines.append(self.text[start : start + len(line)])
+            output_lines.append(text[start : start + len(line)])
             output_lines.append(markers[start : start + len(line)])
             output_lines.append("")
 
