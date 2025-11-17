8a347eac6ed34297fb33965e3309def43be40ff3
Mikhail Pankin <mishapankin@gmail.com>
2025-11-17 14:10:10 +0300

    Changed import logic to support optional dependency

--- src/stt_utils/__init__.py
+++ src/stt_utils/__init__.py
@@ -1,16 +1,3 @@
-from .transcription import (
-    Transcription,
-    UnprocessedTranscription,
-    UnprocessedTimestamp,
-    Timestamp,
-    normalize_word,
-)
-
-try:
-    from .splitter import split_audio_on_silence
-except ImportError:
-    split_audio_on_silence = None
-
 __all__ = [
     "Transcription",
     "UnprocessedTranscription",
@@ -19,3 +6,22 @@ __all__ = [
     "normalize_word",
     "split_audio_on_silence",
 ]
+
+from .transcription import (
+    Transcription,
+    UnprocessedTranscription,
+    UnprocessedTimestamp,
+    Timestamp,
+    normalize_word,
+)
+
+
+def __getattr__(name):
+    if name == "split_audio_on_silence":
+        try:
+            from .splitter import split_audio_on_silence
+
+            return split_audio_on_silence
+        except ImportError as e:
+            raise e
+    raise AttributeError(f"module 'stt_utils' has no attribute '{name}'")
